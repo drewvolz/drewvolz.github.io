69ec6905d6663e36657b3fd0624c6a9a2c11245b
Drew Volz <volz.drew@gmail.com>
2023-07-29 18:35:58 -0700

    add recent github pull request activity section

---
+++ script/github.js
@@ -0,0 +1,57 @@
+const username = 'drewvolz'
+
+const activityContainerParentSelectorID = 'section#gh-activity-container'
+const activityContainerChildSelectorID = 'div#gh-activity'
+const activityContainer = document.querySelector(activityContainerChildSelectorID)
+
+const render = (container, data) => {
+    container.innerHTML = ''
+
+    const dataToRender = data.filter(event => {
+        const {payload, actor, type} = event
+
+        const filterType = ['PullRequestEvent']
+        const isCorrectType = filterType.includes(type)
+        const isCorrectAction = payload.action === 'opened'
+        const isCorrectUser = actor.login === username
+    
+        return isCorrectType && isCorrectAction && isCorrectUser
+    })
+
+    dataToRender.forEach(event => {
+        const {id, type, payload} = event
+
+        const eventContainer = document.createElement('div')
+        eventContainer.setAttribute('data-id', id)
+        eventContainer.setAttribute('data-type', type)
+
+        switch(type) {
+            case 'PullRequestEvent':
+                const pr = payload.pull_request
+                const dateCreated = new Date(pr.created_at).toDateString()
+                const titleText = `"View pull request titled '${pr.title}' in the ${pr.base.repo.name} repo, created ${dateCreated}"`
+                const link = `<a href="${pr.html_url}" title=${titleText} target="_blank">#${pr.number}</a>`
+
+                eventContainer.innerHTML = `<li>${pr.title} (${link})</li>`
+                container.appendChild(eventContainer)
+                break
+            default:
+                console.warn(`unimplemented event type: ${type}`)
+        }
+    })
+}
+
+if (activityContainer) {
+    fetch(`https://api.github.com/users/${username}/events?per_page=100`)
+        .then((response) => {
+            if (!(response.status >= 200 && response.status < 299 || response.status === 304)) {
+                console.warn(`${response.status} status received, hiding recent github activity section`)
+                document.querySelector(activityContainerParentSelectorID).style.display = 'none'
+            }
+
+            return response.json()
+        })
+        .then(data => render(activityContainer, data))
+} else {
+    console.warn(`${activityContainerChildSelectorID} could not be found on the page, doing nothing`)
+}
